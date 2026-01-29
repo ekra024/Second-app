5b5ef2033e7aa6b6273e89ce091c290ace8c24a3
ekra024 <ekra.cse8.bu@gmail.com>
2026-01-29 20:24:13 +0600

    static site generation and dynamicparams

--- src/app/(application)/blogs/[blog]/page.jsx
+++ src/app/(application)/blogs/[blog]/page.jsx
@@ -1,13 +1,21 @@
 import { notFound } from 'next/navigation';
 import React from 'react'
 
+
+
+export const dynamicParams = false;
+
+export async function generateStaticParams(){
+  return {blog: "1"},{blgo:"2"}
+}
+
 export default async function page({params}) {
   const {blog} = await params;
   if(blog != Number(blog)) {
     notFound();
   }
   return (
-    <div>
+    <div className='text-center'>
       <h1>The blog is {blog}</h1>
     </div>
   )
